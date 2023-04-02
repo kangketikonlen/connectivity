be452b2a47ea7f9c68687f9eb8dabb31ec68e8fd
Gilang Pratama <pratamapriadi96@gmail.com>
2023-04-02 23:43:38 +0700

    Change server port & activate update server workflow

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const bootstrapPath = path.join(__dirname, 'node_modules', 'bootstrap', 'dist');
 app.use(express.static('public'));
 app.use('/bootstrap', express.static(bootstrapPath));
 
-app.listen(3000, function () {
-	console.log('Server is listening on port 3000.');
+app.listen(80, function () {
+	console.log('Server is listening on port 80.');
 });
